096caf53c0ee639f30d3622c3860e78ecec9f27d
Aleksandr Konovaltcev <akonovalcev@gmail.com>
2018-09-20 15:50:46 +0400

    started to work

--- Lesson6_Stream3/Program.cs
+++ Lesson6_Stream3/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Даны 2 двумерных матрицы. Размерность 100х100 каждая. Напишите приложение, производящее параллельное умножение матриц. 
+// Матрицы заполняются случайными целыми числами от 0 до 10.
+
 namespace Lesson6_Stream3
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
